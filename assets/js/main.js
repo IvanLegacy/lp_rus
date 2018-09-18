8a7d46ae7fc74ab14c78fd8b2d74966962db354d
Nazar Ugrinovsky <nazar9505@gmail.com>
2018-09-18 13:19:22 +0500

    end mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,11 +23,24 @@ $(function(){
 	let stockSlider = $('.js-slider-stock').owlCarousel();
 
 	stockSlider.on('changed.owl.carousel', function(event) {
-		
+
 		let index = event.property.value;
 		let client = $('.js-slider-stock').find('.slider__item').eq(index).data('client');
 		$('.js-stock-client img').attr('src', client);
 		
+	});
+
+	$('.js-anchors .menu__item').on('click', function () {
+		var scrollAnchor = $(this).attr('data-anchor');
+		var el = $('.js-anchor-section#' + scrollAnchor);
+		if (el && el.length > 0) {
+			var scrollPoint = el.offset().top;
+			$('body,html').animate({
+				scrollTop: scrollPoint
+			}, 500);
+		}
+		$('.js-burger').click();
+		return false;
 	});
 	
 });
